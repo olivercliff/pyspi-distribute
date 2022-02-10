9b3be2eaa1715b18904697663bd94c986211d52e
Oliver Cliff <oliver.m.cliff@gmail.com>
2022-02-10 12:17:39 +1100

    Included MV normal gaussians as sample datasets and a sample YAML file

--- pyspi_compute.py
+++ pyspi_compute.py
@@ -1,10 +1,10 @@
+# Loads the local calculator from calc.pkl, run compute, and save back to file
 import dill
 import os
 
 fname = os.path.dirname(os.path.realpath(__file__)) + '/calc.pkl'
 print(f'Attempting to open: {fname}')
 
-# Simple file to just load the local calculator from calc.pkl, run compute, and save back to file
 with open('calc.pkl', 'rb') as f:
     calc = dill.load(f)
 print(f'Done. Computing...')
