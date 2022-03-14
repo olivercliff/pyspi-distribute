b643a87a157c9c2cf3ce6f1bbb74c03373c385f8
Annie G. Bryant <anniegbryant@gmail.com>
2022-03-14 16:52:48 +1100

    Add options for dimension order and yaml file specification

--- create_yaml_for_samples.R
+++ create_yaml_for_samples.R
@@ -7,22 +7,27 @@ parser <- ArgumentParser(description='Automatically generate sample YAML file fo
 parser$add_argument("--data_dir", help="Directory containing samples' .npy files.")
 parser$add_argument("--sample_metadata", help="OPTIONAL: CSV file containing sample metadata info.")
 parser$add_argument("--label_vars", help="OPTIONAL: columns in metadata to use as labels for YAML.")
+parser$add_argument("--dim_order", help="OPTIONAL: orientation of observations and time points in data. Default is ps, meaning time points are rows and samples are columns.", default="ps")
 parser$add_argument("--overwrite", help="Should sample.yaml be overwritten if it already exists? Default is F.",
                     action="store_true", default=FALSE)
+parser$add_argument("--yaml_file", help="OPTIONAL: Name of output sample YAML file. Default is sample.yaml.",
+                    default="sample.yaml")
 
 # Parse arguments
 args <- parser$parse_args()
 data_dir <- args$data_dir
+dim_order <- args$dim_order
 metadata <- args$sample_metadata
 meta_vars <- args$label_vars
 overwrite <- args$overwrite
+yaml_file_base <- args$yaml_file
 
 if (!endsWith(data_dir, '/')) {
   data_dir <- paste0(data_dir, "/")
 }
 
 npy_files <- list.files(data_dir, pattern="*.npy")
-yaml_file <- paste0(data_dir, "sample.yaml")
+yaml_file <- paste0(data_dir, yaml_file_base)
 
 cat("\nYAML output:", yaml_file, "\n")
 
@@ -33,7 +38,7 @@ if (!is.null(metadata) & !is.null(meta_vars)) {
 if (!file.exists(yaml_file) | overwrite) {
   cat("\nNow creating sample.yaml\n")
   file.create(yaml_file)
-  yaml_string <- "- {file: %s, name: %s, dim_order: sp, labels: [%s] }\n"
+  yaml_string <- "- {file: %s, name: %s, dim_order: %s, labels: [%s] }\n"
   for (npy in npy_files) {
     sample_ID <- tools::file_path_sans_ext(npy)
     if (!is.null(metadata) & !is.null(meta_vars)) {
@@ -48,6 +53,7 @@ if (!file.exists(yaml_file) | overwrite) {
     sample_string <- sprintf(yaml_string, 
                            paste0(data_dir, npy),
                            sample_ID,
+                           dim_order,
                            sample_data_vector)
     write_file(sample_string, yaml_file, append=T)
   }
